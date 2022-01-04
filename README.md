13427a456a09e7abc21132d0bb4a749cb34dc005
adam nunez <adam.a.nunez@gmail.com>
2022-01-03 20:27:10 -0600

    total rewrite

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-**This git repo contains one plain text api key to fetch data from redcap. The repo should never be made public without purging the git history.**
-
 [Site](https://bcwc.surgery.wisc.edu/)
 
-## Technical notes
+## Local Dev Deploy
 
-* navbar lifted from [here](https://codepen.io/j_holtslander/pen/XmpMEp) 
-* Bootstrap 3 was used to support the above
-* Font awesome 4.7 is used for compatability with the above also
+```
+npm install
+npm run dev
+```
 
-## Connecting (for dev)
+## Connecting to Production
 
-* Get a static IP issued by wisc from here
+* Get a static IP issued by wisc from [here](https://access.services.wisc.edu/IPaddress)
 * Reach out to Paul Vreugdenhil for credenitals for a first time connection
-  * Currently there is one end user account on the server named "bcwc".
+  * Currently there is one end user account on the server named "bcwc", credentials are shared.
 * `ssh bcwc@bcwc.surgery.wisc.edu` and install an ssh key like normal. 
 * A symlink in the home directy points to the root of the webserver
-* Any `git commit` will trigger the post-commit git hook and scp all directory files to the server. Git clone from the server is not an option as SSH is disabled
---
+++ public/proxy.php
@@ -0,0 +1,36 @@
+<?php
+$token = $_ENV['REDCAP_TEST_TOKEN'];
+$url = 'https://redcap.surgery.wisc.edu/redcap/api/';
+$fields = ['demo_gender', 'demo_race', 'demo_ethnicity'];
+
+$data = [
+    'token' => $token,
+    'content' => 'record',
+    'action' => 'export',
+    'format' => 'json',
+    'type' => 'flat',
+    'csvDelimiter' => '',
+    'fields' => $fields,
+    'rawOrLabel' => 'raw',
+    'rawOrLabelHeaders' => 'raw',
+    'exportCheckboxLabel' => 'false',
+    'exportSurveyFields' => 'false',
+    'exportDataAccessGroups' => 'false',
+    'returnFormat' => 'json'
+];
+#$ch = curl_init();
+#curl_setopt($ch, CURLOPT_URL, $url);
+#curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+#curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+#curl_setopt($ch, CURLOPT_VERBOSE, 0);
+#curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+#curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+#curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+#curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+#curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+#curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+#$response = curl_exec($ch);
+#curl_close($ch);
+
+header('Content-Type: application/json; charset=utf-8');
+echo $response; # Redcap sends back json
