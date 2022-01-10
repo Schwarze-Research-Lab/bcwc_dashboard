ab3732520e95c1792a9472043588972be53710f9
adam nunez <adam.a.nunez@gmail.com>
2022-01-09 18:18:24 -0600

    Table and datepicker (non functional)

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ import Chart from 'chart.js/auto'
 import * as d3 from 'd3-scale-chromatic'
 
 let redcap = [];
-let content = document.getElementById("content");
 const site_map = {
-    999: 'Unknown',
     94: 'C',
     95: 'J',
     96: 'M',
@@ -14,7 +12,8 @@ const site_map = {
     98: 'P',
     99: 'WA',
     100: 'V',
-    101: 'WV'
+    101: 'WV',
+    999: 'Unknown',
 };
 init();
 
@@ -39,9 +38,11 @@ function init() {
     };
 
     // Setup Nav
+    const content = document.getElementById("content");
     const allButtons = document.querySelectorAll("#nav-bar a, #nav-bar-mobile a");
     const activeClass = ["bg-gray-900", "text-white"];
     const defaultClass = ["text-gray-300", "hover:bg-gray-700", "hover:text-white"];
+
     allButtons.forEach(btn => {
         btn.onclick = el => {
             // Style all butons to normal
@@ -65,39 +66,42 @@ function init() {
 function buildDashboard() {
 
     // Page hashes and other static content are defined in HTML
-    enrollment(document.getElementById('enrollmentSummary'));
+
+    let data = getEnrollemntData();
+    console.log(data);
+    enrollment(document.getElementById('enrollmentSummary'), data);
+    siteEnrollmentTable(document.getElementById('siteEnrollmentTable'), data);
+    timeSeriesEnrollment(document.getElementById('timeSeriesEnrollment'), data);
 
 };
 
-function enrollment(element) {
-    let enro = getEnrollemntData();
-    console.log(enro);
+function enrollment(element, data) {
 
     // Build Summary Table
-    let table = element.getElementsByTagName('table')[0];
-    insert2colRow(table, "Screened", enro.screened);
-    insert2colRow(table, "Elligible", enro.elligible);
-    insert2colRow(table, "Enrolled", enro.enrolled);
-    insert2colRow(table, "Enrolled <sm>(30days)<sm>", enro.last_30_enrolled);
-    insert2colRow(table, "Study Age", Math.round(enro.months_study_active, 1) + "<sm>months<sm>");
+    const table = element.getElementsByTagName('table')[0];
+    insert2colRow(table, "Screened", data.screened);
+    insert2colRow(table, "Elligible", data.elligible);
+    insert2colRow(table, "Enrolled", data.enrolled);
+    insert2colRow(table, "Enrolled <sm>(30days)<sm>", data.last_30_enrolled);
+    insert2colRow(table, "Study Age", Math.round(data.months_study_active, 1) + "<sm>months<sm>");
 
     // Default Color Options
     const colorScale = d3.interpolateRainbow;
     const colorRangeInfo = {
         colorStart: 0,
-        colorEnd: 1,
+        colorEnd: 0.25,
         useEndAsStart: false,
     };
 
     // Generate the chart
-    const labels = Object.keys(enro.site).map(x => site_map[x]);
+    const labels = Object.keys(data.site).map(x => site_map[x]);
     const colors = interpolateColors(labels.length, colorScale, colorRangeInfo);
     const config = {
         type: 'doughnut',
         data: {
             labels: labels,
             datasets: [{
-                data: Object.entries(enro.site).map(x => x[1].enrolled),
+                data: Object.entries(data.site).map(x => x[1].enrolled),
                 backgroundColor: colors,
                 hoverOffset: 4
             }]
@@ -105,8 +109,61 @@ function enrollment(element) {
     };
 
     // Paint to screen
-    let canvas = element.getElementsByTagName('canvas')[0];
-    let chart = new Chart(canvas, config);
+    const canvas = element.getElementsByTagName('canvas')[0];
+    new Chart(canvas, config);
+}
+
+function siteEnrollmentTable(element, data) {
+
+    // Set up
+    let cell;
+    let cssTable = element.getElementsByClassName('grid')[0];
+    cssTable.style.gridTemplateColumns = `repeat(${Object.values(site_map).length + 2}, minmax(0, 1fr))`;
+
+    // Remove anything that was previously added
+    var target = cssTable.getElementsByTagName("div"), index;
+    for (index = target.length - 1; index >= 0; index--) {
+        target[index].parentNode.removeChild(target[index]);
+    }
+
+    // Headers
+    cell = document.createElement('div');
+    cell.innerHTML = `<b></b>`;
+    cssTable.appendChild(cell);
+    Object.values(site_map).forEach(siteText => {
+        cell = document.createElement('div');
+        cell.innerHTML = `<b>${siteText}</b>`;
+        cssTable.appendChild(cell);
+    });
+    cell = document.createElement('div');
+    cell.innerHTML = `<b>Total</b>`;
+    cssTable.appendChild(cell);
+
+    ["Screened", "Elligible", "Enrolled"].forEach(title => {
+        cell = document.createElement('div');
+        cell.innerHTML = `<b>${title}</b>`;
+        cssTable.appendChild(cell);
+        let total = 0;
+        Object.entries(site_map).forEach(entry => {
+            let [siteCode, siteText] = entry;
+            cell = document.createElement('div');
+            cell.innerHTML = `<b>0</b>`;
+            cssTable.appendChild(cell);
+            if (data.site[siteCode]) {
+                let tmp = data.site[siteCode][title.toLowerCase()];
+                cell.innerHTML = `<b>${tmp || 0}</b>`;
+                total += tmp || 0;
+            }
+        });
+        cell = document.createElement('div');
+        cell.innerHTML = `<b>${total}</b>`;
+        cssTable.appendChild(cell);
+    });
+
+}
+
+function timeSeriesEnrollment(element, data) {
+
 }
 
 function getEnrollemntData() {
@@ -139,52 +196,53 @@ function getEnrollemntData() {
         const data = redcap[id];
         const month = data.screen_datetime.split('-').slice(0, 2).join('-');
         const date = data.screen_datetime.split(' ')[0];
+        const screenSite = data.screen_site || 999;
 
         // Time series data Init to 0
         !(date in o.time_series) && (o.time_series[date] = {});
-        !(o.time_series[date][data.screen_site]) && (o.time_series[date][data.screen_site] = {});
-        !(o.time_series[date][data.screen_site].screened) && (o.time_series[date][data.screen_site].screened = 0);
-        !(o.time_series[date][data.screen_site].enrolled > -1) && (o.time_series[date][data.screen_site].enrolled = 0);
-        !(o.time_series[date][data.screen_site].elligible > -1) && (o.time_series[date][data.screen_site].elligible = 0);
-        o.time_series[date][data.screen_site]['screened'] += 1
+        !(o.time_series[date][screenSite]) && (o.time_series[date][screenSite] = {});
+        !(o.time_series[date][screenSite].screened) && (o.time_series[date][screenSite].screened = 0);
+        !(o.time_series[date][screenSite].enrolled > -1) && (o.time_series[date][screenSite].enrolled = 0);
+        !(o.time_series[date][screenSite].elligible > -1) && (o.time_series[date][screenSite].elligible = 0);
+        o.time_series[date][screenSite]['screened'] += 1
 
         // Site indexed data
-        !(o.site[data.screen_site]) && (o.site[data.screen_site] = {});
-        !(o.site[data.screen_site].screened) && (o.site[data.screen_site].screened = 0);
-        !(o.site[data.screen_site].enrolled) && (o.site[data.screen_site].enrolled = 0);
-        !(o.site[data.screen_site].months_enrolled) && (o.site[data.screen_site].months_enrolled = {});
-        !(o.site[data.screen_site].date_of_first_screen) && (o.site[data.screen_site].date_of_first_screen = "3000-01-01");
-        o.site[data.screen_site].screened += 1;
+        !(o.site[screenSite]) && (o.site[screenSite] = {});
+        !(o.site[screenSite].screened) && (o.site[screenSite].screened = 0);
+        !(o.site[screenSite].enrolled) && (o.site[screenSite].enrolled = 0);
+        !(o.site[screenSite].months_enrolled) && (o.site[screenSite].months_enrolled = {});
+        !(o.site[screenSite].date_of_first_screen) && (o.site[screenSite].date_of_first_screen = "3000-01-01");
+        o.site[screenSite].screened += 1;
 
-        if (data.screen_datetime < o.site[data.screen_site].date_of_first_screen) {
-            o.site[data.screen_site].date_of_first_screen = data.screen_datetime;
+        if (data.screen_datetime < o.site[screenSite].date_of_first_screen) {
+            o.site[screenSite].date_of_first_screen = data.screen_datetime;
         }
 
         // Enrolled into sutdy
         if (data.rescreen_me == "1") {
             o.enrolled += 1;
-            o.time_series[date][data.screen_site]['enrolled'] += 1;
+            o.time_series[date][screenSite]['enrolled'] += 1;
 
             const last30Days = ((new Date()).getTime() - (new Date(data.screen_datetime)).getTime()) < DAYS_30;
             if (last30Days) {
                 o.last_30_enrolled += 1;
             }
 
-            o.site[data.screen_site].enrolled += 1;
+            o.site[screenSite].enrolled += 1;
             !(o.month_enrolled[month]) && (o.month_enrolled[month] = 0);
             o.month_enrolled[month] += 1;
-            !(o.site[data.screen_site].months_enrolled[month]) && (o.site[data.screen_site].months_enrolled[month] = 0);
-            o.site[data.screen_site].months_enrolled[month] += 1;
+            !(o.site[screenSite].months_enrolled[month]) && (o.site[screenSite].months_enrolled[month] = 0);
+            o.site[screenSite].months_enrolled[month] += 1;
 
-            if (data.screen_datetime > (o.site[data.screen_site].most_recent_enrollment || "")) {
-                o.site[data.screen_site].most_recent_enrollment = data.screen_datetime;
+            if (data.screen_datetime > (o.site[screenSite].most_recent_enrollment || "")) {
+                o.site[screenSite].most_recent_enrollment = data.screen_datetime;
             }
         }
 
         // Last minute stuff
         if (["1", "2", "3"].includes(data.decision_final)) {
             o.elligible += 1;
-            o.time_series[date][data.screen_site]['elligible'] += 1;
+            o.time_series[date][screenSite]['elligible'] += 1;
         }
 
         if (data.screen_datetime < o.date_of_first_screen) {
@@ -201,19 +259,20 @@ function getEnrollemntData() {
     return o;
 }
 
-function calculatePoint(i, intervalSize, colorRangeInfo) {
-    var { colorStart, colorEnd, useEndAsStart } = colorRangeInfo;
-    return (useEndAsStart
-        ? (colorEnd - (i * intervalSize))
-        : (colorStart + (i * intervalSize)));
-}
-
+// Util Func - create an RBG color array for graphs
 function interpolateColors(dataLength, colorScale, colorRangeInfo) {
     var { colorStart, colorEnd } = colorRangeInfo;
     var colorRange = colorEnd - colorStart;
     var intervalSize = colorRange / dataLength;
     var i, colorPoint;
     var colorArray = [];
+
+    function calculatePoint(i, intervalSize, colorRangeInfo) {
+        var { colorStart, colorEnd, useEndAsStart } = colorRangeInfo;
+        return (useEndAsStart
+            ? (colorEnd - (i * intervalSize))
+            : (colorStart + (i * intervalSize)));
+    }
 
     for (i = 0; i < dataLength; i++) {
         colorPoint = calculatePoint(i, intervalSize, colorRangeInfo);
@@ -223,6 +282,7 @@ function interpolateColors(dataLength, colorScale, colorRangeInfo) {
     return colorArray;
 }
 
+// Util Func - build a row with left and right col in table
 function insert2colRow(table, left, right) {
     let row = table.insertRow();
     let cell1 = row.insertCell(0);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,6 @@ module.exports = {
     extend: {
     }
   },
-  plugins: []
+  plugins: [
+  ]
 }
